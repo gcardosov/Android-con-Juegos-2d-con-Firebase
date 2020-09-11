1bbce438efb55b53fe8647fb94c4b8a515c5f875
Gerardo Cardoso Vera <ger9047@gmail.com>
2020-09-11 16:18:19 -0500

    Gestion game over

--- app/src/main/java/com/example/duckhunt/GameActivity.java
+++ app/src/main/java/com/example/duckhunt/GameActivity.java
@@ -25,6 +25,7 @@ public class GameActivity extends AppCompatActivity {
     int anchoPantalla;
     int altoPantalla;
     Random aleatorio;
+    boolean gameOver = false;
 
 
     @Override
@@ -49,9 +50,48 @@ public class GameActivity extends AppCompatActivity {
 
             public void onFinish() {
                 tvTimer.setText( "0s");
+                gameOver = true;
+                mostrarDialogoGameOver();
             }
         }.start();
 
+
+    }
+
+    private void mostrarDialogoGameOver() {
+        //Cuadro de dialogo
+        //salir del juego a jugar de nuevo
+        //usamos el componmente dialog
+        // 1. Instantiate an  with its constructor
+        //nos permite configurar el cuadro de dialogo
+        //recibe como parametro el contexto
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        //escribimos el mensaje
+        builder.setMessage("Has conseguido cazar " + counter + " patos")
+                .setTitle("Game over");
+
+        //agreagamos los botones antes de generar el builder
+        builder.setPositiveButton("Reiniciar el juego" new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                dialog.dismiss();
+            }
+        });
+
+        // 3. Get the from create()
+        //genera el alertDialog
+        //a partir del Builder
+        AlertDialog dialog = builder.create();
+        //el metodo show lo muestra
+
+        //4. Mostrar el dialogo
+        dialog.show();
     }
 
     private void initPantalla() {
@@ -91,19 +131,22 @@ public class GameActivity extends AppCompatActivity {
                 ivDuck.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //Que queremos hacer al dar click en el pato
-                        //Incrementamos el contador con el numero de patos
-                        counter++;
-                        tvCounterDucks.setText(String.valueOf(counter));
-                        //cambiar la imagen del pato
-                        ivDuck.setImageResource(R.drawable.duck_clicked);
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                ivDuck.setImageResource(R.drawable.duck);
-                                moveDuck();
-                            }
-                        }, 500);
+                        if (!gameOver) {
+                            //Que queremos hacer al dar click en el pato
+                            //Incrementamos el contador con el numero de patos
+                            counter++;
+                            tvCounterDucks.setText(String.valueOf(counter));
+                            //cambiar la imagen del pato
+                            ivDuck.setImageResource(R.drawable.duck_clicked);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ivDuck.setImageResource(R.drawable.duck);
+                                    moveDuck();
+                                }
+                            }, 500);
+
+                        }
 
                     }
                 });
